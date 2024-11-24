33c751466e23e08993833615d0be6911f0f5753a
JeonSeungHyeok <victory_hyeok@naver.com>
2024-11-24 23:20:23 +0900

    parser init

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
 from scanner import *
-import optparse
-import sys
-from color import *
+import argparse
 
-def option_help():
-    help = f'''
-{GREEN}Usage{RESET}: port scanner
+def add_options(parser):
+    parser.add_argument('-stl', action='store_true', dest='stealth', help="stealth scan")
     
-{GREEN}Option help{RESET}:
-    -h      scan option
-{GREEN}Scan method{RESET}:
-    -S      TCP SYN scan
-    -A      TCP ACK scan
-    -N      TCP Null scan
-    -X      TCP X-mas scan
-{GREEN}IP & Port{RESET}:
-    -IP     Specify IP range 
-    -P      Specify Port range
-{GREEN}Speed{RESET}:
-    -T      Specify Thread 0~10
-    '''
-    print(help)
-def parser():
-    pass
-
-
+    parser.add_argument('-S', action='store_true', dest='SYN', help="TCP SYN scan")
+    parser.add_argument('-A', action='store_true', dest='ACK', help="TCP ACK scan")
+    parser.add_argument('-N', action='store_true', dest='Null', help="TCP Null scan")
+    parser.add_argument('-X', action='store_true', dest='Xmas', help="TCP X-mas scan")
+    
+    parser.add_argument('-IP', action='append', dest='ip', default=socket.gethostbyname(socket.getfqdn()), help="Specify IP range(basic IP is self)")
+    parser.add_argument('-P', action='append', dest='port', default=range(1024), help="Specify Port range")
+    
+    parser.add_argument('-T', action='count', dest='thread', default=0, help="Specify Thread 0~10")
+    
+    parser.add_argument('-OS', action='store_true', dest='os', help="Detection OS")
+    
+    parser.add_argument('-oj', action='store_true', dest='output_json', help="Output JSON")
+    parser.add_argument('-ox', action='store_true', dest='output_xml', help="Output XML")
+    
 
 def main():
-    option_help()
-    command = 'a'#input("'scanner' 및 'scanner -h' 및 'scanner --help'를 입력해주세요!")
-    if command in ('scan', 'scan -h', 'scan --help'):
-        option_help()
-    else:
-        print('다시 입력해주세요')
-    
-    
+    parser = argparse.ArgumentParser()
+    add_options(parser)
+    options, args = parser.parse_args()
+    print(options, args)
+
 
 if __name__ == '__main__':
     main()
