45bafed09855bd2b28ed4c7234685628a29342a1
JeonSeungHyeok <victory_hyeok@naver.com>
2024-11-25 18:21:15 +0900

    m

--- xmas_scan.py
+++ xmas_scan.py
@@ -3,10 +3,10 @@ from scapy.all import IP, TCP, sr1, send
 
 def scan_xmas_port(targetIp, port):
     """단일 포트에 대해 TCP Xmas 스캔 수행"""
-    xmasPacket = IP(dst=targetIp) / TCP(dport=port, flags="FPU")  # FIN, PSH, URG 플래그 설정
+    xmasPacket = IP(dst=targetIp) / TCP(dport=port, flags="FPU")
     response = sr1(xmasPacket, timeout=1, verbose=0)
 
     if response:
         if response.haslayer(TCP) and response[TCP].flags == "R":
             return port, "Closed"
-    return port, "Open or Filtered"  # 응답 없음
+    return port, "Open or Filtered"
