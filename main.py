8c3270dfa694a4ae333aae2a9dd471a775566151
Potato030 <126748489+Potato030@users.noreply.github.com>
2024-11-25 14:55:37 +0900

    Update "parser option"

--- main.py
+++ main.py
@@ -18,9 +18,18 @@ def add_options(parser):
     """명령줄 인자를 추가하는 함수"""
     parser.add_argument('-S', action='store_true', dest='syn', help="TCP SYN scan")
     parser.add_argument('-A', action='store_true', dest='ack', help="TCP ACK scan")
+    parser.add_argument('-N', action='store_true', dest='Null', help="TCP Null scan")
+    parser.add_argument('-X', action='store_true', dest='Xmas', help="TCP X-mas scan")
+    
     parser.add_argument('-IP', required=True, dest='ip', help="Specify target IP")
     parser.add_argument('-P', required=True, dest='port', help="Specify port range (e.g., '22,80,443' or '20-30')")
+    
     parser.add_argument('-T', type=int, default=1, dest='threads', help="Specify number of threads (default: 1)")
+
+    parser.add_argument('-OS', action='store_true', dest='os', help="Detection OS")
+    
+    parser.add_argument('-oj', action='store_true', dest='output_json', help="Output JSON")
+    parser.add_argument('-ox', action='store_true', dest='output_xml', help="Output XML")
 
 def perform_scan(scanFunction, targetIp, ports, numThreads, filterStatus):
     """스캔을 쓰레드를 활용해 병렬로 수행"""
