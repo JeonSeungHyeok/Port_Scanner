790b91d279f33fd16fd40f1cae5c456d66529221
SEOGEONWOO <gotfh3@naver.com>
2024-11-26 10:56:32 +0900

    feat : 스캔 결과 출력
    스캔 결과 출력

--- thread.py
+++ thread.py
@@ -58,7 +58,7 @@ class Thread:
 
     def print_result(self, results: list, startTime: time) -> None:
         # 결과 정렬 및 출력
-        filteredResults = [result for result in results if result[1] == "필터링되지 않음 (RST 수신)" or result[1] == 'Open']
+        filteredResults = [result for result in results if result[1] == "필터링되지 않음 (RST 수신)" or result[1] == 'Open or Filtered']
         filteredResults.sort(key=lambda x: x[0])
 
         print("\n[LOG] 스캔 결과 출력:")
