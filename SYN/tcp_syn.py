4569d6b61c42bec78f748250f91e171ee10c5659
YH <yunghun0312@gmail.com>
2024-11-27 13:39:23 +0900

    syn-scan random port

--- SYN/tcp_syn.py
+++ SYN/tcp_syn.py
@@ -1,9 +1,16 @@
 from scapy.all import IP, TCP, sr1, send
+import random
 
-def scan_syn_port(targetIp, port,timeout,max_tries):
+def scan_syn_port(targetIp, port, timeout, maxTries):
     """단일 포트에 대해 TCP SYN 스캔 수행"""
-    synPacket = IP(dst=targetIp) / TCP(dport=port, flags="S")
-    response = sr1(synPacket, timeout=timeout, verbose=0)
+    for i in range(maxTries):
+        srcPort = random.randint(10000, 65535)
+        synPacket = IP(dst=targetIp) / TCP(sport=srcPort, dport=port, flags="S")
+        response = sr1(synPacket, timeout=timeout, verbose=0)
+        if response is None:
+            continue
+        else:
+            break
 
     if response:
         if response.haslayer(TCP) and response[TCP].flags == "SA":
