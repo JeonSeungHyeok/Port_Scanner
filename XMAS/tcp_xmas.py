cf343a0d03295688aa633f2bb9528d9e69e41225
SEOGEONWOO <gotfh3@naver.com>
2024-11-27 21:39:54 +0900

    feat : tcp_xmas.py 추가
    tcp_xmas.py 재 추가

---
+++ XMAS/tcp_xmas.py
@@ -0,0 +1,18 @@
+from scapy.all import IP, TCP, sr1
+import random
+
+def scan_xmas_port(targetIp, port,timeout,maxTries):
+    """단일 포트에 대해 TCP Xmas 스캔 수행"""
+    for i in range(maxTries):
+        srcPort = random.randint(10000, 65535)
+        xmasPacket = IP(dst=targetIp) / TCP(sport=srcPort, dport=port, flags="FPU")
+        response = sr1(xmasPacket, timeout=timeout, verbose=0)
+        if response is None:
+            continue
+        else:
+            break
+
+    if response:
+        if response.haslayer(TCP) and response[TCP].flags == "R":
+            return port, "Closed"
+    return port, "Open or Filtered"
