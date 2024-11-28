c2c5e1cecb39adc15dd1837f77b658862a205a78
JeonSeungHyeok <victory_hyeok@naver.com>
2024-11-28 16:50:21 +0900

    http banner test

--- service_version.py
+++ service_version.py
@@ -19,11 +19,8 @@ def get_basic_banner(targetIp, port, timeout):
     try:
         with socket.create_connection((targetIp, port), timeout) as sock:
             # 서비스 응답을 읽음
+            sock.sendall(b"\r\n")  # 간단한 핑 신호 전송
             banner = sock.recv(1024).decode(errors="ignore").strip()
-            if not banner:
-                sock.sendall(b"\r\n")  # 간단한 핑 신호 전송
-                banner = sock.recv(1024).decode(errors="ignore").strip()
-
             return banner
     except (socket.timeout, ConnectionRefusedError, OSError):
         return "No Banner"
@@ -35,18 +32,28 @@ def get_ssl_banner(targetIp, port, timeout):
     try:
         with socket.create_connection((targetIp, port), timeout) as sock:
             # 서비스 응답을 읽음
-            sock.sendall(b"\r\n")  # 간단한 핑 신호 전송
-            banner = sock.recv(1024).decode().strip()
+            sock.sendall(b"HEAD / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n" % targetIp.encode())  # 간단한 핑 신호 전송
+            response = sock.recv(1024).decode(errors="ignore").strip()
+            banner = extract_server_header(response)
+
             return banner
     except (socket.timeout, ConnectionRefusedError, OSError):
         return "No Banner"
-
+def extract_server_header(response):
+    """
+    HTTP 응답에서 Server 헤더만 추출하는 함수
+    """
+    headers = response.split("\r\n")
+    for header in headers:
+        if header.lower().startswith("server:"):
+            return header
+    return None
     
 def scan_service_version(targetIp, port, timeout, maxTries):
     result = scan_syn_port(targetIp, port, timeout, maxTries)
     if result[1] == "Open":
         if port == 80 or port == 443:
-            pass
+            return result[0], result[1], get_service_name(port), get_ssl_banner(targetIp, port, timeout)
         else:
             return result[0], result[1], get_service_name(port), get_basic_banner(targetIp, port, timeout)
     return result[0], result[1], None, None
