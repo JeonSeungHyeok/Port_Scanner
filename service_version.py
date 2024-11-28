f2027fd75d901476526aeeae0011d85168f87a3a
JeonSeungHyeok <victory_hyeok@naver.com>
2024-11-28 14:31:35 +0900

    test

--- service_version.py
+++ service_version.py
@@ -2,17 +2,7 @@ import socket
 from scapy.all import IP, TCP, sr1, send
 from SYN.tcp_syn import *
 
-def get_service_name(targetIp, port, timeout):
-
-    srcPort = random.randint(10000, 65535) # 랜덤 소스 포트 설정
-    synPacket = IP(dst=targetIp) / TCP(sport=srcPort, dport=port, flags="S")
-    response = sr1(synPacket, timeout=timeout, verbose=0)
-    ackPacket = IP(dst=targetIp)/TCP(sport=srcPort, dport=port, flags='A', seq=response[TCP].ack, ack=response[TCP].seq+1)
-    send(ackPacket, verbose=False)
-
-    with socket.create_connection((targetIp, port), timeout=timeout) as sock:
-        sock.sendall(b"\r\n")
-        banner = sock.recv(1024).decode(errors="ignore").strip()
+def get_service_name(port):
     """
     주어진 포트 번호에 대한 서비스 이름을 반환.
     알 수 없는 포트의 경우 'unknown' 반환.
@@ -22,7 +12,7 @@ def get_service_name(targetIp, port, timeout):
     except OSError:
         return "unknown"
     
-def get_banner(targetIp, port, timeout=2):
+def get_banner(targetIp, port, timeout):
     """
     특정 IP와 포트에서 배너 정보를 가져옴
     """
@@ -33,11 +23,10 @@ def get_banner(targetIp, port, timeout=2):
             banner = sock.recv(1024).decode().strip()
             return banner
     except (socket.timeout, ConnectionRefusedError, OSError):
-        return "No Banner"
+        return "No Banner11"
     
 def scan_service_version(targetIp, port, timeout, maxTries):
     result = scan_syn_port(targetIp, port, timeout, maxTries)
     if result[1] == "Open":
-
-        return result[0], result[1], get_service_name(targetIp, port, timeout), get_banner(targetIp, port, timeout)
+        return result[0], result[1], get_service_name(port), get_banner(targetIp, port, timeout)
     return result[0], result[1], None, None
