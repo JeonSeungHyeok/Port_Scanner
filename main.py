bed5d85c22dfaac5f0fd590a20f6156fa728e097
SEOGEONWOO <gotfh3@naver.com>
2024-11-29 21:10:39 +0900

    faet :  output_json 추가
    output_json 추가

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from option import *
 from thread import *
 import argparse
+import sys
+import time
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(usage=usage_msg())
     add_options(parser)
-    options = parser.parse_args()
-    thread = Thread(ip=options.ip,port=options.port,timeout=options.time,numThread=options.threads,maxTries=options.tries,scanMethod=option(options))
+    if hasattr(parser, 'parse_intermixed_args'):
+        options = parser.parse_intermixed_args()
+    else:
+        options = parser.parse_args()
+
+   
+    output_json = options.output_json and options.service_version
+    thread = Thread(ip=options.ip,port=options.port,timeout=options.time,numThread=options.threads,maxTries=options.tries,os=options.os,scanMethod=option(options),output_json=options.output_json)
     result, time = thread.start_thread()
-    thread.print_result(result,time)
+    thread.print_result(result)
 
 if __name__=="__main__":
     main()
