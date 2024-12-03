35c28600d505994eb99c0481e336e8450756ca4a
JeonSeungHyeok <victory_hyeok@naver.com>
2024-12-03 16:14:06 +0900

    modify p0f

--- OS/p0f.py
+++ OS/p0f.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from colors import *
 import subprocess
 import time
+import sys
 import os
 
 def run_docker_p0f(log_dir, target_ip):
@@ -14,7 +15,7 @@ def run_docker_p0f(log_dir, target_ip):
         'p0f',target_ip
     ]
     
-    print(f"{BLUE}[*]{RESET} Ececuting Docker Container {YELLOW}p0f{RESET}")
+    print(f"{BLUE}[*]{RESET} Excuting  Docker Container {YELLOW}p0f{RESET} for {YELLOW}{target_ip}{RESET}")
     try:
         p0f_proc = subprocess.Popen(docker_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, universal_newlines=True)
     except FileNotFoundError:
@@ -23,28 +24,31 @@ def run_docker_p0f(log_dir, target_ip):
     except Exception as e:
         print(f"{RED}[-]{RESET} Error while ececuting Docker Container: {e}")
         return "Unknown"
-    
-    try:
-        time.sleep(2)
-        print(f"{BLUE}[*]{RESET} Sending HTTP Request to {YELLOW}{target_ip}{RESET}")
-        subprocess.run(['curl', target_ip], check=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
-        print(f"{BLUE}[*]{RESET} HTTP Request sended")
-        time.sleep(3)
-        
-    except subprocess.CalledProcessError as e:
-        print(f"{RED}[-]{RESET} Error while sending HTTP Request: {e}")
-    finally:
-        stdout, stderr = p0f_proc.communicate()
-        if stdout:
-            print(f"Docker stdout:\n{stdout}")
-        if stderr:
-            print(f"Docker stderr:\n{stderr}")
-    log_file = log_dir_path / 'p0f_output.log'
-    os_info = extract_os_info(log_file)
-    return os_info
+    time.sleep(3)
+    #max_retries = 3
+    #for attempt in range(max_retries):
+    #    try:
+    #        subprocess.run(['curl', f'{target_ip}:80'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    #        print(f"{BLUE}[*]{RESET} HTTP Request sent successfully to {YELLOW}{target_ip}{RESET}")
+    #        break
+    #    except subprocess.CalledProcessError as e:
+    #        if attempt < max_retries - 1:
+    #            print(f"{RED}[-]{RESET} Retry {attempt + 1}/{max_retries}")
+    #            time.sleep(2)
+    #        else:
+    #            print(f"{RED}[-]{RESET} HTTP Request failed: {e}")
+    #    finally:
+    #        stdout, stderr = p0f_proc.communicate(timeout=10)
+    #        if stdout:
+    #            print(f"Docker stdout:\n{stdout}")
+    #        if stderr:
+    #            print(f"Docker stderr:\n{stderr}")
+    log_file = log_dir_path / f'{target_ip}_p0f_output.log'
+    osInfo = extract_os_info(log_file)
+    return dict(ip=target_ip,OS=osInfo)
 
 def extract_os_info(log_file_path):
-    os_info = "Unknown"
+    osInfo = "Unknown"
     try:
         with open(log_file_path, 'r') as log_file:
             lines = log_file.readlines()
@@ -53,13 +57,20 @@ def extract_os_info(log_file_path):
                 if '|os' in line:
                     count += 1
                     if count == 2:
-                        os_info = line.split('=')[5].strip().split('|')[0]
+                        osInfo = line.split('=')[5].strip().split('|')[0]
                         break
     except FileNotFoundError:
         print(f"{RED}[-]{RESET} Can't find log file: {YELLOW}{log_file_path}{RESET}")
     except Exception as e:
         print(f"{RED}[-]{RESET} Error while reading log file: {e}")
-    os.remove(log_file_path)
-    print(f"{BLUE}[*]{RESET} {YELLOW}{log_file_path}{RESET} deleted")
+    try:
+        os.remove(log_file_path)
+        print(f"{BLUE}[*]{RESET} {YELLOW}{log_file_path}{RESET} deleted")
+    except FileNotFoundError:
+        print(f"{RED}[-]{RESET} Can't delete log file: {YELLOW}{log_file_path}{RESET}")
     
-    return os_info
+    return osInfo
+
+def print_os_info(osInfo:list):
+    for OS in osInfo:
+        print(f"{BLUE}[*]{RESET} OS detected at {YELLOW}{OS['ip']}{RESET} : {YELLOW}{OS['OS']}{RESET}")
