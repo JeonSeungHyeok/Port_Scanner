c3c3a20f24fd522fd6bfd68b1fdc301538dce16b
JeonSeungHyeok <victory_hyeok@naver.com>
2024-12-03 23:24:40 +0900

    thread

--- thread.py
+++ thread.py
@@ -40,24 +40,27 @@ class Thread:
         if ',' in ip:
             ipList = []
             ipListTmp = ip.split(',')
+            
             for ips in ipListTmp:
                 funcedIp = self.ip_range_to_list(ips)
                 ipList.extend(funcedIp)
             return ipList
+        
         if '-' in ip:
             try:
-                start_ip_str, end_ip_str = ip.split('-')
-                start_ip = ipaddress.IPv4Address(start_ip_str.strip())
-                end_ip = ipaddress.IPv4Address(end_ip_str.strip())
-
-                if start_ip > end_ip:
-                    raise ValueError("Start IP must be less than or equal to End IP.")
+                startIpStr, endIpStr = ip.split('-')
+                startIp = ipaddress.IPv4Address(startIpStr.strip())
+                endIp = ipaddress.IPv4Address(endIpStr.strip())
                 ipList = []
-                for ip_int in range(int(start_ip), int(end_ip) + 1):
-                    ipList.append(str(ipaddress.IPv4Address(ip_int)))
+
+                if startIp > endIp:
+                    raise ValueError('Start IP must be less than or equal to End IP.')
+                
+                for ipInt in range(int(startIp), int(endIp) + 1):
+                    ipList.append(str(ipaddress.IPv4Address(ipInt)))
                 return ipList
             except Exception as e:
-                print(f"Error: {e}")
+                print(f'Error: {e}')
                 return []
         elif '/' in ip:
             net4 = ipaddress.ip_network(ip)  
@@ -68,12 +71,14 @@ class Thread:
                 
     def parse_ports(self, portInput: str) -> list:  # 포트 범위를 파싱하여 섞인 포트 목록 반환
         ports = []
+        
         for part in portInput.split(','):
             if '-' in part:
                 start, end = map(int, part.split('-'))
                 ports.extend(range(start, end + 1))  # range 결과를 리스트에 추가
             else:
                 ports.append(int(part.strip()))  # 개별 포트를 리스트에 추가
+
         random.shuffle(ports)  # shuffle을 통해 보안 시스템의 연속적인 포트 스캔 탐지를 우회
         return ports
     
@@ -81,13 +86,13 @@ class Thread:
         conf.verb=0
         ipList = self.ip_range_to_list(self.ip)
 
+        osResult={}
         if self.os: 
-            osResult=[]
-            with ThreadPoolExecutor(max_workers=self.numThread,) as executor:
+            with ThreadPoolExecutor(max_workers=self.numThread) as executor:
                 futures = [executor.submit(run_docker_p0f, os.getcwd(), ip) for ip in ipList]
                 for future in as_completed(futures):
-                    osResult.append(future.result())
-            print_os_info(osResult)
+                    osResult.update(future.result())
+            #print_os_info(osResult)
         ports = self.parse_ports(self.port)
         scanMethods = {
             'syn':scan_syn_port,
@@ -99,32 +104,43 @@ class Thread:
 
         for ip in ipList:
             results=[]
-            with ThreadPoolExecutor(max_workers=self.numThread,) as executor:
-                futures = [executor.submit(scanMethods.get(self.scanMethod), ip, port, self.timeout,self.maxTries) for port in ports]
+            with ThreadPoolExecutor(max_workers=self.numThread) as executor:
+                futures = [executor.submit(scanMethods.get(self.scanMethod), ip, port, self.timeout, self.maxTries) for port in ports]
                 for future in as_completed(futures):
                     results.append(future.result())
-            self.print_result(ip=ip,results=results)
+            filteredResults = [result for result in results if result[1] == 'Unfiltered (RST received)' or result[1]=='Open' or result[1]=='Open or Filtered']
+            filteredResults.sort(key=lambda x: x[0])
+                    
+            portCveList = {}
+            if self.cve:
+                for port, state, *_ in filteredResults:
+                    cveData = shodan_api(self.ip, port, self.timeout, self.maxTries).process()
+                    portCveList[port] = cveData if cveData else []
 
-    def print_result(self, ip: str, results: list)->None:      # 스캔 결과를 출력하는 메서드
-        filteredResults = [result for result in results if result[1] == 'Unfiltered (RST received)' or result[1]=='Open' or result[1]=='Open or Filtered']
-        filteredResults.sort(key=lambda x: x[0])
-        print(f"\n{YELLOW}{self.scanMethod.upper()} Scan{RESET} Result of {YELLOW}{ip}{RESET} :")
+            self.print_result(ip=ip,results=filteredResults,osResult=osResult, portCveList=portCveList)
+
+    def print_result(self, ip: str, results: list,osResult, portCveList)->None:      # 스캔 결과를 출력하는 메서드
+        if self.os:
+            print(f"\n{YELLOW}{self.scanMethod.upper()} Scan{RESET} Result of {YELLOW}{ip}{RESET}: {osResult['OS']}")
+        else:
+            print(f"\n{YELLOW}{self.scanMethod.upper()} Scan{RESET} Result of {YELLOW}{ip}{RESET}: ")
 
         if self.scanMethod == 'version':
             print(f"\n{'Result':^82}")
             print('='*82)
             print(f"{'PORT':<10}{'STATE':<20}{'SERVICE':<20}{'BANNER'}")
-            for port, state, service, banner in filteredResults:
+            
+            for port, state, service, banner in results:
                 print(f"Port {port}: {state:<20}{service or 'N/A':<20}{banner or 'N/A'}")
         else:
-            for port, state in filteredResults:
+            for port, state in results:
                 print(f'Port {port}: {state}')
         
         if self.cve:
-            for port in filteredResults:
+            for port in results:
                 print(f'CVE List at Port {port[0]}: {shodan_api(ip, port[0], self.timeout, self.maxTries).process()}')
 
         if self.outputFile:        
-            save_result_as_json(ip, filteredResults, self.scanMethod, self.outputFile)
+            save_result_as_json(ip, results, self.scanMethod, self.outputFile, osResult, portCveList)
         if self.outputXml:
-            save_result_as_xml(ip, filteredResults, self.scanMethod, self.outputXml)
+            save_result_as_xml(ip, results, self.scanMethod, self.outputXml, osResult, portCveList)
